925a99b304780de2dc99356abe16e6fbb191bae8
Daniel Waggoner <daxxon37@hotmail.com>
2017-08-07 18:43:06 -0400

    added more content

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 let myBody = '';
+let myContainer = '';
 let myHeader = '';
 let headerH1 = '';
+let headerH2Basics = '';
+let headerH2Story = '';
 let theBasics = '';
+let basicsP1 = '';
+let basicsP2 = '';
+let basicsP3 = '';
+let basicsP4 = '';
+let basicsP5 = '';
 let theStory = '';
+let storyP = '';
 let userIcon = '';
-let token = {
-  daxxon:'34701b01614fe7de8fae776746bc4473188fe671'
-};
 let name = '';
+let username = '';
 let gitHubUrl = '';
 let emailAddress = '';
 let company = '';
@@ -20,14 +27,34 @@ function renderPageElements() {
   myBody.appendChild(myHeader);
   headerH1 = document.createElement('h1');
   myHeader.appendChild(headerH1);
+  myContainer = document.createElement('div');
+  myContainer.id = 'infoBox';
+  myBody.appendChild(myContainer);
   theBasics = document.createElement('div');
   theBasics.id = 'theBasics';
-  theBasics.appendChild(document.createElement('h3'));
+  myContainer.appendChild(theBasics);
+  headerH2Basics = document.createElement('h2');
+  theBasics.appendChild(headerH2Basics);
+  basicsP1 = document.createElement('p');
+  theBasics.appendChild(basicsP1);
+  basicsP2 = document.createElement('p');
+  theBasics.appendChild(basicsP2);
+  basicsP3 = document.createElement('p');
+  theBasics.appendChild(basicsP3);
+  basicsP4 = document.createElement('p');
+  theBasics.appendChild(basicsP4);
+  basicsP5 = document.createElement('p');
+  theBasics.appendChild(basicsP5);
   theStory = document.createElement('div');
   theStory.id = 'theStory';
-  theStory.appendChild(document.createElement('h3'));
+  myContainer.appendChild(theStory);
+  headerH2Story = document.createElement('h2');
+  theStory.appendChild(headerH2Story);
+  storyP = document.createElement('p');
+  theBasics.appendChild(storyP);
   userIcon = document.createElement('div');
-
+  userIcon.id = 'userIcon';
+  myContainer.appendChild(userIcon);
 }
 
 function requestUserData() {
@@ -40,7 +67,8 @@ function requestUserData() {
 function populateData() {
   let userData = JSON.parse(this.responseText);
   console.log(userData);
-  name = userData.login;
+  name = userData.name;
+  userName = userData.login;
   gitHubUrl = userData.html_url;
   emailAddress = userData.email;
   company = userData.company;
@@ -49,7 +77,14 @@ function populateData() {
 }
 
 function populatePage(userData) {
-  headerH1.innerHTML = userData.name;
+  headerH1.innerHTML = `${name}`;
+  headerH2Basics.innerHTML = "The Basics";
+  headerH2Story.innerHTML = "The Story";
+  basicsP1.innerHTML = `Name: ${name}`;
+  basicsP2.innerHTML = `Github URL: <a href="${gitHubUrl}">${userName}</a>`
+  basicsP3.innerHTML = `Email: ${emailAddress}`;
+  basicsP4.innerHTML = `Company: ${company}`
+  basicsP5.innerHTML = `Website: ${website}`
 }
 
 renderPageElements();
